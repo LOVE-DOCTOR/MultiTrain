647370f6f48e01880c235bd8cb959c6d0b0d305c
LOVE-DOCTOR <18-52ha137@gmail.com>
2022-06-01 01:29:47 +0100

    Added new classification models

--- classification/classification_models.py
+++ classification/classification_models.py
@@ -6,10 +6,11 @@ from sklearnex import patch_sklearn
 from sklearnex.linear_model import LogisticRegression
 from sklearn.linear_model import SGDClassifier, PassiveAggressiveClassifier
 from sklearnex.ensemble import RandomForestClassifier
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import GradientBoostingClassifier, HistGradientBoostingClassifier, AdaBoostClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 from catboost import CatBoostClassifier
 from xgboost import XGBClassifier
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB, CategoricalNB, ComplementNB
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearnex.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
@@ -17,11 +18,14 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import warnings
+warnings.filterwarnings("ignore")
 
 patch_sklearn()
 
 
-def split(X: any, y: any, strat: bool = False, sizeOfTest: float = 0.2, randomState: int = None, shuffle_data: bool = True) -> tuple[any, any, any, any]:
+def split(X: any, y: any, strat: bool = False, sizeOfTest: float = 0.2, randomState: int = None,
+          shuffle_data: bool = True) -> tuple[any, any, any, any]:
     if isinstance(X, int) or isinstance(y, int):
         raise ValueError(f"{X} and {y} are not valid arguments for 'split'."
                          f"Try using the standard variable names e.g split(X, y) instead of split({X}, {y})")
@@ -53,7 +57,7 @@ def split(X: any, y: any, strat: bool = False, sizeOfTest: float = 0.2, randomSt
 class Models:
 
     def __init__(self, lr=0, sgdc=0, pagg=0, rfc=0, gbc=0, cat=0, xgb=0, gnb=0, lda=0, knc=0, mlp=0, svc=0,
-                 dtc=0) -> None:
+                 dtc=0, bnb=0, mnb=0, cnb=0, conb=0, hgbc=0, abc=0, etc=0) -> None:
         """
         :param lr: Logistic Regression
         :param sgdc: Stochastic Gradient Descent Classifier
@@ -67,6 +71,12 @@ class Models:
         :param knc: KNeighborsClassifier
         :param mlp: MLPClassifier
         :param svc: Support Vector Classifier
+        :param dtc: DecisionTreeClassifier
+        :param cnb: CategoricalNB
+        :param conb: ComplementNB
+        :param hgbc: HistGradientBoostingClassifier
+        :param abc: AdaBoostClassifier
+        :param etc: ExtraTreesClassifier
         """
 
         self.lr = lr
@@ -82,15 +92,26 @@ class Models:
         self.mlp = mlp
         self.svc = svc
         self.dtc = dtc
+        self.bnb = bnb
+        self.mnb = mnb
+        self.cnb = cnb
+        self.conb = conb
+        self.hgbc = hgbc
+        self.abc = abc
+        self.etc = etc
 
     def initialize(self):
         self.lr = LogisticRegression(random_state=42, warm_start=True, max_iter=400)
-        self.sgdc = SGDClassifier(random_state=42)
+        self.sgdc = SGDClassifier(random_state=42, early_stopping=True, validation_fraction=0.2,
+                                  shuffle=True, n_iter_no_change=20)
         self.pagg = PassiveAggressiveClassifier(shuffle=True, fit_intercept=True, early_stopping=True,
                                                 validation_fraction=0.1, n_iter_no_change=20, n_jobs=-1)
         self.rfc = RandomForestClassifier(random_state=42, warm_start=True)
         self.gbc = GradientBoostingClassifier(random_state=42, learning_rate=0.01, validation_fraction=0.1,
                                               n_iter_no_change=20)
+        self.hgbc = HistGradientBoostingClassifier(early_stopping=True, validation_fraction=0.2, random_state=42,
+                                                   max_iter=300)
+        self.abc = AdaBoostClassifier(random_state=42)
         self.cat = CatBoostClassifier(random_state=42, learning_rate=0.01)
         self.xgb = XGBClassifier(use_label_encoder=False)
         self.gnb = GaussianNB()
@@ -100,7 +121,14 @@ class Models:
                                  early_stopping=True, n_iter_no_change=20)
         self.svc = SVC(random_state=42)
         self.dtc = DecisionTreeClassifier(random_state=42)
-        return self.lr, self.sgdc, self.pagg, self.rfc, self.gbc, self.cat, self.xgb, self.gnb, self.lda, self.knc, self.mlp, self.svc, self.dtc
+        self.bnb = BernoulliNB()
+        self.mnb = MultinomialNB()
+        self.cnb = CategoricalNB()
+        self.conb = ComplementNB()
+        self.etc = ExtraTreesClassifier(warm_start=True, random_state=42, n_jobs=-1)
+
+        return (self.lr, self.sgdc, self.pagg, self.rfc, self.gbc, self.hgbc, self.abc, self.cat, self.xgb, self.gnb,
+                self.lda, self.knc, self.mlp, self.svc, self.dtc, self.bnb, self.mnb, self.cnb, self.conb, self.etc)
 
     def fit_eval_models(self, X_train=None, X_test=None, y_train=None, y_test=None,
                         split_data: str = None, splitting: bool = False):
