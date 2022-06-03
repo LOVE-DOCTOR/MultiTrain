51fc1793e866c79b6a401968c54828e17c740fc4
LOVE-DOCTOR <18-52ha137@gmail.com>
2022-06-03 20:55:50 +0100

    removed some comments

--- LOGGING/log_message.py
+++ LOGGING/log_message.py
@@ -8,17 +8,12 @@ def __log():
 
 
 def PrintLog(message):
-    # form = logging.Formatter("%(asctime)s : %(levelname)-5.5s : %(message)s")
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
-    # console_handler_format = '%(acstime)s | %(levelname)s: %(message)s'
-    # console_handler.setFormatter(logging.Formatter(console_handler_format))
     logger = __log()
     logger.addHandler(console_handler)
     logger.info(message)
 
 
 
-# def WarnLog(message):
-#    logging.basicConfig(level=logging.WARNING)
-#    logging.warning(me)
+
