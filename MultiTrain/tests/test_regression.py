e558ac47ea1693d3f0d21cbc62bef12eff989e0d
haddyadnan <haddyadnan@gmail.com>
2022-09-30 16:10:34 +0100

    added test structure

--- MultiTrain/tests/test_regression.py
+++ MultiTrain/tests/test_regression.py
@@ -3,18 +3,34 @@ test cases for Classification class
 """
 
 import unittest
+import pandas as pd
+import numpy as np
 
 # import MultiTrain
-from MultiTrain.classification import classification_models
+from MultiTrain.regression.regression_models import MultiRegressor
 
-X = [[1, 0], [0, 1], [1, 1]]
-Y1 = [0.3, 2.3, 1.8]
+df = pd.DataFrame(
+    {"X": np.arange(0, 10), "X2": np.linspace(2, 3, 10), "Y": np.linspace(0.1, 1, 10)}
+)
+features = df.drop("Y", axis=1)
+target = round(df.Y, 2)
+train_reg = MultiRegressor(
+    cores=-1,  # this parameter works exactly the same as setting n_jobs to -1, this uses all the cpu cores to make training faster
+    random_state=42,  # setting random state here automatically sets a unified random state across function imports
+)
+split = train_reg.split(
+    X=features, y=target, sizeOfTest=0.1, randomState=42, shuffle_data=True
+)
 
 
-class testClassification(unittest.TestCase):
+class testRegression(unittest.TestCase):
     """
     Test Regression methods
     """
 
     def test_fit(self):
-        pass
+        """Check return type"""
+        fitted_reg = train_reg.fit(
+            splitting=True, split_data=split, show_train_score=True, excel=True
+        )
+        self.assertEqual(pd.DataFrame, type(fitted_reg))
