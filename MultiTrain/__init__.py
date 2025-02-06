b67ef12fb4b1b0e6f94d193866e1ffb145251e00
love-doctor <tunexo885@gmail.com>
2025-02-06 20:22:36 +0100

    improve workflow

--- MultiTrain/__init__.py
+++ MultiTrain/__init__.py
@@ -1,2 +1,3 @@
 from MultiTrain.classification.classification_models import MultiClassifier
 from MultiTrain.regression.regression_models import MultiRegressor
+from MultiTrain.__version__ import __version__
